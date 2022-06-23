7cebcdbdb9df96b9ebf55bcde4a1befe2081e917
mask-smirnov <mask.smirnov@gmail.com>
2022-06-24 01:59:58 +0300

    int, float and double constructors 4 triangle

--- MindboxAreaCalc/Figure.cs
+++ MindboxAreaCalc/Figure.cs
@@ -15,9 +15,12 @@
             throw new Exception(String.Format("Type {0} is not supported", _type));
         }
 
-        public static Figure Construct(string _type, int _dimension1) => Figure.Construct(_type, (decimal)_dimension1);
-        public static Figure Construct(string _type, float _dimension1) => Figure.Construct(_type, (decimal)_dimension1);
-        public static Figure Construct(string _type, double _dimension1) => Figure.Construct(_type, (decimal)_dimension1);
+        public static Figure Construct(string _type, int _dimension1) 
+            => Figure.Construct(_type, (decimal)_dimension1);
+        public static Figure Construct(string _type, float _dimension1) 
+            => Figure.Construct(_type, (decimal)_dimension1);
+        public static Figure Construct(string _type, double _dimension1) 
+            => Figure.Construct(_type, (decimal)_dimension1);
 
         public static Figure Construct(string _type, decimal _dimension1, decimal _dimension2, decimal _dimension3) //конструктор фигур с тремя параметрами
         {
@@ -28,6 +31,12 @@
 
             throw new Exception(String.Format("Type {0} is not supported", _type));
         }
+        public static Figure Construct(string _type, int _dimension1, int _dimension2, int _dimension3) 
+            => Figure.Construct(_type, (decimal)_dimension1, (decimal)_dimension2, (decimal)_dimension3);
+        public static Figure Construct(string _type, float _dimension1, float _dimension2, float _dimension3)
+            => Figure.Construct(_type, (decimal)_dimension1, (decimal)_dimension2, (decimal)_dimension3);
+        public static Figure Construct(string _type, double _dimension1, double _dimension2, double _dimension3)
+            => Figure.Construct(_type, (decimal)_dimension1, (decimal)_dimension2, (decimal)_dimension3);
 
     }
 }
